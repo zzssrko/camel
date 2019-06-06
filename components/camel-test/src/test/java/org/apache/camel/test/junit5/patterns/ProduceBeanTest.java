4ca3f50b3ab671279208c4fc3c1279a8d4ef9833
aldettinger <aldettinger@gmail.com>
2019-06-06 15:27:20 +0200

    CAMEL-13342: Added a JUnit 5 version of JUnit 4 test patterns

--- components/camel-test/src/test/java/org/apache/camel/test/junit5/patterns/ProduceBeanTest.java
+++ components/camel-test/src/test/java/org/apache/camel/test/junit5/patterns/ProduceBeanTest.java
@@ -14,39 +14,33 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.camel.test.junit5;
+package org.apache.camel.test.junit5.patterns;
 
-import org.apache.camel.ProducerTemplate;
-import org.apache.camel.RoutesBuilder;
 import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.component.mock.MockEndpoint;
-import org.apache.camel.model.ModelCamelContext;
+import org.apache.camel.test.junit5.CamelTestSupport;
 import org.junit.jupiter.api.Test;
 
-@CamelTest
-public class SimpleMockTest {
+/**
+ *
+ */
+public class ProduceBeanTest extends CamelTestSupport {
 
     @Test
-    public void testMock(ProducerTemplate template, ModelCamelContext ctx) throws Exception {
-        MockEndpoint mockResult = ctx.getEndpoint("mock:result", MockEndpoint.class);
-        mockResult.expectedBodiesReceived("Hello World");
+    public void testProduceBean() throws Exception {
+        getMockEndpoint("mock:result").expectedMessageCount(1);
+
         template.sendBody("direct:start", "Hello World");
-        MockEndpoint.assertIsSatisfied(ctx);
+
+        assertMockEndpointsSatisfied();
     }
 
-    /* Same test is shorter with junit4
-    @Test
-    public void testMock() throws Exception {
-        getMockEndpoint("mock:result").expectedBodiesReceived("Hello World");
-        template.sendBody("direct:start", "Hello World");
-        assertMockEndpointsSatisfied();
-    }*/
-
-    protected RoutesBuilder createRouteBuilder() throws Exception {
+    @Override
+    protected RouteBuilder createRouteBuilder() throws Exception {
         return new RouteBuilder() {
             @Override
             public void configure() throws Exception {
-                from("direct:start").to("mock:result");
+                from("direct:start")
+                    .bean(MyProduceBean.class, "doSomething");
             }
         };
     }
